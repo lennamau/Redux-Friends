5e373e3034103f5a794ef3d9df7604d70d40e066
Lenna <lenna@lennamau.com>
2019-01-31 14:44:42 -0700

    index set up, starting app

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.getFriends();
+  }
   render() {
     return (
       <div className="App">
